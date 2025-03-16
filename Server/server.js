a3418c183864d13d0097d04755c5701c1e204d16
udaykumar2507 <uk25072004@gmail.com>
2025-03-16 12:24:01 +0530

    cors  check

--- Server/server.js
+++ Server/server.js
@@ -3,23 +3,15 @@ const dotenv = require('dotenv');
 const cors = require('cors');
 const connectDB = require('./config/db');
 
-require('./models/User');  
-require('./models/Course');
-
-const authRoutes = require('./routes/authRoutes');
-const courseRoutes = require('./routes/courseRoutes');
-
 dotenv.config();
 connectDB();
 
 const app = express();
 
-
 const allowedOrigins = [
   'http://localhost:5173',
-  'https://course-management-system-r82o.vercel.app'
+  'https://course-management-system-gray.vercel.app'
 ];
-
 
 const corsOptions = {
   origin: function (origin, callback) {
@@ -29,18 +21,21 @@ const corsOptions = {
       callback(new Error('Not allowed by CORS'));
     }
   },
-  credentials: true
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 };
 
-
 app.use(cors(corsOptions));
-
+app.options('*', cors(corsOptions));
 
 app.use(express.json());
 
+const authRoutes = require('./routes/authRoutes');
+const courseRoutes = require('./routes/courseRoutes');
+
 app.use('/api/auth', authRoutes);
 app.use('/api/courses', courseRoutes);
-
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
