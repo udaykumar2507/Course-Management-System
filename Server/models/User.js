023dc624c2074b508f57541e00a74aeecdc53541
udaykumar2507 <uk25072004@gmail.com>
2025-04-11 22:38:18 +0530

    instructor spelling changed

--- Server/models/User.js
+++ Server/models/User.js
@@ -6,7 +6,7 @@ const userSchema = new mongoose.Schema({
   password: { type: String, required: true },
   role: {
     type: String,
-    enum: ['student','admin','Instructor'],
+    enum: ['student','admin','instructor'],
     default: 'student',
   },
   profilephoto: {
